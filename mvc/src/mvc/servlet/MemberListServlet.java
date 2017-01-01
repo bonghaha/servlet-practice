32bb8092a36256afa77cb1eec5f3b6d37817f762
Bongha Hwang <qhdgk825@naver.com>
2017-01-01 09:59:36 +0900

    commit test

---
+++ mvc/src/mvc/servlet/MemberListServlet.java
@@ -0,0 +1,62 @@
+package mvc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/member/list")
+public class MemberListServlet extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost/mvc", //JDBC URL
+					"root",			// DBMS 사용자 아이디
+					"java0000");	// DBMS 사용자 암호
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(
+					"SELECT mno,mname,email,cre_date" + 
+					" FROM members" +
+					" ORDER BY mno ASC");
+			
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<html><head><title>회원목록</title></head>");
+			out.println("<body><h1>회원목록</h1>");
+			out.println("<p><a href='add'>신규 회원</a></p>");
+			while(rs.next()) {
+				out.println(
+					rs.getInt("mno") + "," +
+					"<a href='update?mno=" + rs.getInt("mno") + "'>" + rs.getString("mname") + "</a>," +
+					rs.getString("email") + "," + 
+					rs.getDate("cre_date") + "<br>"
+				);
+			}
+			out.println("</body></html>");
+		} catch (Exception e) {
+			throw new ServletException(e);
+			
+		} finally {
+			try {if (rs != null) rs.close();} catch(Exception e) {}
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+			try {if (conn != null) conn.close();} catch(Exception e) {}
+		}
+
+	}
+}
---
+++ mvc/src/mvc/servlet/MemberUpdateServlet.java
@@ -0,0 +1,91 @@
+package mvc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@SuppressWarnings("serial")
+@WebServlet("/member/update")
+public class MemberUpdateServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			ServletContext sc = this.getServletContext();
+			Class.forName(sc.getInitParameter("driver"));
+			conn = DriverManager.getConnection(
+					this.getInitParameter("url"),
+					this.getInitParameter("username"),
+					this.getInitParameter("password"));
+			
+			stmt = conn.createStatement();
+			String query = "SELECT mno, email, mname, cre_date FROM members WHERE mno = " + request.getParameter("mno");
+			rs = stmt.executeQuery(query);
+			rs.next();
+			
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<html><head><title>회원정보</title></head>");
+			out.println("<body><h1>회원정보</h1>");
+			out.println("<form action='update' method='post'>");
+			out.println("번호 : <input type='text' name='mno' value='" + request.getParameter("mno") + "'readonly><br>");
+			out.println("이름 : <input type='text' name='mname' value='" + rs.getString("mname") + "'><br>");
+			out.println("이메일 : <input type='text' name='email' value='" + rs.getString("email") + "'><br>");
+			out.println("가입일 : " + rs.getDate("cre_date") + "<br>");
+			out.println("<input type='submit' value='저장'>");
+			out.println("<input type='button' value='취소' onclick='location.href=\"list\"'>");
+			out.println("</form>");
+			out.println("</body></html>");
+		} catch (Exception e) {
+			throw new ServletException(e);
+		} finally {
+			try {if(rs != null) rs.close();} catch(Exception e) {}
+			try {if(stmt != null) stmt.close();} catch(Exception e) {}
+			try {if(conn != null) conn.close();} catch(Exception e) {}
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			Class.forName(this.getInitParameter("driver"));
+			conn = DriverManager.getConnection(
+					this.getInitParameter("url"),
+					this.getInitParameter("username"),
+					this.getInitParameter("password"));
+			String query = "UPDATE members SET email=?, mname=?, mod_date=now() WHERE mno=?";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, request.getParameter("email"));
+			pstmt.setString(2, request.getParameter("mname"));
+			pstmt.setInt(3, Integer.parseInt(request.getParameter("mno")));
+			pstmt.executeUpdate();
+			response.sendRedirect("list");
+			
+		} catch (Exception e) {
+			throw new ServletException(e);
+		} finally {
+			try {if(pstmt != null) pstmt.close();} catch(Exception e) {}
+			try {if(conn != null) conn.close();} catch(Exception e) {}
+		}
+	}
+}
