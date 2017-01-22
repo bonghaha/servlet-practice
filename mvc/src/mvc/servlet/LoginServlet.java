818e044bf7539e34d994f77f00bec04b9a9b8a23
bonghaha <qhdgk825@naver.com>
2017-01-23 07:12:49 +0900

    Using Dao for checking email and password

--- mvc/src/mvc/servlet/LoginServlet.java
+++ mvc/src/mvc/servlet/LoginServlet.java
@@ -2,8 +2,6 @@ package mvc.servlet;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -14,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import mvc.dao.MemberDao;
 import mvc.vo.Member;
 
 @SuppressWarnings("serial")
@@ -29,23 +28,16 @@ public class LoginServlet extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 		
 		try {
 			ServletContext sc = this.getServletContext();
 			conn = (Connection) sc.getAttribute("conn");
-			String query = "SELECT mname, email FROM members WHERE email=? AND pwd=?";
-			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, request.getParameter("email"));
-			pstmt.setString(2, request.getParameter("password"));
-			rs = pstmt.executeQuery();
 			
-			if(rs.next()) {
-				Member member = new Member()
-						.setEmail(rs.getString("email"))
-						.setMname(rs.getString("mname"));
-				
+			MemberDao memberDao = new MemberDao();
+			memberDao.setConnection(conn);
+			Member member = memberDao.exist(request.getParameter("email"), request.getParameter("password"));
+			
+			if(member != null) {
 				HttpSession session = request.getSession();
 				session.setAttribute("member", member);
 				
@@ -58,9 +50,6 @@ public class LoginServlet extends HttpServlet{
 		} catch(Exception e) {
 			throw new ServletException(e);
 			
-		} finally {
-			try {if(rs != null) rs.close();} catch(Exception e) {}
-			try {if(pstmt != null) pstmt.close();} catch(Exception e) {}
 		}
 	}
 }
